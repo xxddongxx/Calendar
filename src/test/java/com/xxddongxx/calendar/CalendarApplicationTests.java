e41f9242b3276bbedbf17700da352b44d61faf01
xxddongxx <hyeon2187@gmail.com>
2025-04-15 02:44:45 +0900

    Calendar 프로젝트 세팅
    - mybatis
    - logback
    - profiles
    - swagger

---
+++ src/test/java/com/xxddongxx/calendar/CalendarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xxddongxx.calendar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalendarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
